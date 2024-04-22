0f1c18318075c98d9b74f0ed60b17984ea2d8497
neuroLena <lena.egor@gmail.com>
2024-04-23 03:35:26 +1200

    fixstyle of  barplot legend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
-import React, {useState, useEffect} from "react";
-import logoRU from "./../assets/logoRU.svg"; 
-import logoEN from "./../assets/logoEN.svg"; 
-
-import {ButtonGroupLang} from "./shared/ButtonGroup";
-import { useLanguage } from '../contexts/langContext';
-import { getBannerData } from "../services/googleSheetsService";
 
 
-export default function Header() {
-  const {language, setLanguage} = useLanguage(); 
-  const logo = language === 'ru' ? logoRU : logoEN;
-  
-  const Banner = ({ year, link }) => {
-    return (
+import React, {useState, useEffect} from "react"; 
+import logoRU from "./../assets/logoRU.svg";  
+import logoEN from "./../assets/logoEN.svg";  
+ 
+import {ButtonGroupLang} from "./shared/ButtonGroup"; 
+import { useLanguage } from '../contexts/langContext'; 
+import { getBannerData } from "../services/googleSheetsService"; 
+ 
+ 
+export default function Header() { 
+  const {language, setLanguage} = useLanguage();  
+  const logo = language === 'ru' ? logoRU : logoEN; 
+   
+  const Banner = ({ year, link }) => { 
+    if (!banner || banner.status !== 'on') { 
+      return null; // Don't display if no active config 
+    } 
+   
+    return ( 
       <a href={link} target="_blank" rel="noopener noreferrer" className="banner">
         <div>
       
@@ -22,61 +28,58 @@ export default function Header() {
       
           </div>
       </a>
-    );
-  };
-  
-    const [banner, setBanner] = useState(null);
-  
-    useEffect(() => {
-      getBannerData().then(setBanner);
-    }, []);
-  
-    if (!banner || banner.status !== 'on') {
-      return null; // Don't display if no active config
-    }
-  
-  
-  
-  const changeLanguage = (event) => {
-    setLanguage(event.target.name);
-  };
-
-  return (
-    <header className="header">
-      <div className="App-header-logo">
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className="banner-header">
-        <Banner year={banner.year} link={banner.link} />
-      </div>
-      <div className="buttin-group-header">
-        <div
-          className="header-buttons-container"
-          style={{ marginLeft: "20px" }}
-        >
-          <a
-            href="https://comingoutspb.com/we-are-helping/"
-            className="button-secondary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {language === "ru" ? "Нужна помощь" : "Need Help"}
-          </a>
-          <a
-            href="https://comingoutspb.org/support/"
-            className="button-primary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {language === "ru" ? "Хочу помочь" : "Want to Help"}
-          </a>
-        </div>
-
-        <ButtonGroupLang
-          buttons={["ru", "en"]}
-          onButtonClick={changeLanguage}
-        />
-      </div>
-    </header>
-  );
+    ); 
+  }; 
+   
+    const [banner, setBanner] = useState(null); 
+   
+    useEffect(() => { 
+      getBannerData().then((res) => setBanner(res)); 
+    }, []); 
+   
+    
+   
+   
+  const changeLanguage = (event) => { 
+    setLanguage(event.target.name); 
+  }; 
+ 
+  return ( 
+    <header className="header"> 
+      <div className="App-header-logo"> 
+        <img src={logo} alt="Logo" /> 
+      </div> 
+      <div className="banner-header"> 
+        <Banner year={banner?.year} link={banner?.link} /> 
+      </div> 
+      <div className="buttin-group-header"> 
+        <div 
+          className="header-buttons-container" 
+          style={{ marginLeft: "20px" }} 
+        > 
+          <a 
+            href="https://comingoutspb.com/we-are-helping/" 
+            className="button-secondary" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+          > 
+            {language === "ru" ? "Нужна помощь" : "Need Help"} 
+          </a> 
+          <a 
+            href="https://comingoutspb.org/support/" 
+            className="button-primary" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+          > 
+            {language === "ru" ? "Хочу помочь" : "Want to Help"} 
+          </a> 
+        </div> 
+ 
+        <ButtonGroupLang 
+          buttons={["ru", "en"]} 
+          onButtonClick={changeLanguage} 
+        /> 
+      </div> 
+    </header> 
+  ); 
 }
